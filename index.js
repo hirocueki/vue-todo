4bb910374780cc30a40024855e45dde8bda8cbfd
Hiroshi Ueki <hirocueki@gmail.com>
2020-11-16 10:03:09 +0900

    add ESLint & Pretter

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+import Vue from 'vue'
+
 Vue.createApp({
-data(){
-  return {
-    message: "TODO apps"
-  }
-}
-}).mount("#app")
+  data() {
+    return {
+      message: 'TODO apps',
+    }
+  },
+}).mount('#app')
