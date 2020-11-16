57ca805fd98cc6559d44c6a9b22aab1f0d664049
Hiroshi Ueki <hirocueki@gmail.com>
2020-11-16 10:56:58 +0900

    todo作成、作成チェックの関数追加

--- app.js
+++ app.js
@@ -8,11 +8,36 @@ Vue.createApp({
       selectCategory: '',
       hideDoneTodo: false,
       order: 'desc',
+      categoryName: '',
     }
   },
   computed: {
     canCreateTodo() {
       return this.todoTitle !== ''
+    },
+    canCreateCategory() {
+      return this.categoryName !== ''
+    },
+  },
+  methods: {
+    createTodo() {
+      if (!this.canCreateTodo) {
+        return
+      }
+
+      // Todoを追加する
+      this.todoTitle = ''
+      this.todoDescription = ''
+      this.todoCategories = []
+    },
+    canCreateCategory() {
+      if (!this.canCreateCategory) {
+        return
+      }
+
+      // カテゴリを追加する
+
+      this.categoryName = ''
     },
   },
 }).mount('#app')
