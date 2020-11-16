3f87bb926db0930cdbaf2f5b6d42b173136f3c7f
Hiroshi Ueki <hirocueki@gmail.com>
2020-11-16 14:30:53 +0900

    add modal files

---
+++ modal.js
@@ -0,0 +1,21 @@
+Vue.createApp({
+  data() {
+    return {
+      // サムネイル配列
+      thumbnails: [
+        {
+          id: 1,
+          src: 'https://placehold.jp/300x300.png',
+        },
+        {
+          id: 2,
+          src: 'https://placehold.jp/3d4070/ffffff/300x300.png',
+        },
+        {
+          id: 3,
+          src: 'https://placehold.jp/b32023/ffffff/300x300.png',
+        },
+      ],
+    }
+  },
+}).mount('#app')
